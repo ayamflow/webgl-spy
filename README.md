f976d4ee8d3983766cdb239f30eb7701f41c53d2
Florian Morel <ayamflow@gmail.com>
2025-03-31 15:58:34 +0800

    Initial commit & alpha version

---
+++ README.md
@@ -0,0 +1,61 @@
+# WebGL Spy
+
+A small utility to monitor WebGL2 drawcalls and other performance-related methods.
+Like a pocket-sized SpectorJS for quick peek at your pipeline.
+
+## Usage
+`npm i webgl-spy`
+
+```js
+import WebglSpy from 'webgl-spy'
+
+const spy = new WebglSpy(context)
+// three.js: new WebglSpy(renderer.getContext())
+// ogl: new WebglSpy(renderer.gl)
+// babylon: new WebglSpy(engine._gl)
+
+function render() {
+    requestAnimationFrame(update)
+
+    spy.startCapture()
+    renderer.render(scene, camera)
+    const calls = spy.endCapture()
+}
+```
+
+`calls` is an array of `{ draw, program? }` listing at minima the draw method name and the shader name, if available.
+Other info can be
+- clear bits (depth, stencil, color)
+- draw indices count / vertices count
+
+### API
+#### new WebglSpy(context)
+Creates a new instance to monitor the given WebGL2RenderingContext.
+
+#### startCapture()
+Call before any drawing operation (setRenderTarget, clear, etc).
+
+#### endCapture( onlyDraws = false )
+Call after everything has been rendered. Returns the list of calls.
+
+#### destroy()
+Restore the context by removing spies. Probably not needed.
+
+## To do
+This is a simple proof of concept and might not get developed further.
+If it happens, some ideas:
+- show framebuffer read/write/id
+- add state calls (i.e. enable/disable scissor, stencil, cull_face, depth_test, viewport, clearColor, blitFrameBuffer...)
+- Typescript for encapsulation & types
+- Unit tests
+
+## Credits
+[BabylonJS' great SpectorJS extension](https://github.com/BabylonJS/Spector.js) for a lot of the ideas, constants & spy pattern implementation.
+
+[threejs](https://github.com/mrdoob/three.js) for the draw methods (WebGLIndexedBufferRenderer.js and WebGLBufferRenderer.js)
+
+[spite](https://gist.github.com/spite/7ae92212b4f28076ba29) for getting everyone to implement the `#define SHADER_NAME` hint
+
+[Active Theory](https://activetheory.net/)'s Hydra for the console.log idea
+
+## Unlicense
---
+++ index.js
@@ -0,0 +1,167 @@
+const SPY_OG_PREFIX = 'WEBGL_SPY_'
+
+// Taken from 
+// https://github.com/mrdoob/three.js WebGLIndexedBufferRenderer.js
+// and WebGLBufferRenderer.js
+const SPY_METHODS = [
+    'clear',
+    'bindFramebuffer',
+    'drawElements',
+    'drawElementsInstanced',
+    'multiDrawElementsWEBGL',
+    'multiDrawElementsInstancedWEBGL',
+    'drawArrays',
+    'drawArraysInstanced',
+    'multiDrawArraysWEBGL',
+    'multiDrawArraysInstancedWEBGL',
+]
+
+const NO_PROGRAM = [
+    'clear',
+    'bindFramebuffer',
+]
+
+const DEPTH_BUFFER_BIT = 256
+const STENCIL_BUFFER_BIT = 1024
+const COLOR_BUFFER_BIT = 16384
+const DRAW_MODES = ['POINTS', 'LINES', 'LINE_LOOP', 'LINE_STRIP', 'TRIANGLES', 'TRIANGLE_FAN']
+const CURRENT_PROGRAM = 35725
+
+export default class WebglSpy {
+    constructor(gl) {
+        this.gl = gl
+        this.calls = []
+        this.hasSpied = false
+    }
+
+    spyContext(methods) {
+        const gl = this.gl
+        methods.forEach(methodName => {
+            const ogMethod = gl[methodName]
+            gl[SPY_OG_PREFIX + methodName] = ogMethod
+            gl[methodName] = (...args) => {
+                ogMethod.apply(gl, args)
+                const details = getDetails(gl, methodName, args)
+                this.calls.push(details)
+            }
+        })
+    }
+
+    unspyContext() {
+        const gl = this.gl
+        Object.getOwnPropertyNames(gl).forEach(propName => {
+            if (propName.indexOf(SPY_OG_PREFIX) === 0) {
+                const methodName = propName.replace(SPY_OG_PREFIX, '')
+
+                // Cleanup all property functions
+                // will default back to prototype methods
+                gl[methodName] = null
+                delete gl[methodName]
+                gl[propName] = null
+                delete gl[propName]
+            }
+        })
+    }
+
+    startCapture(reset = true) {
+        if (!this.hasSpied) {
+            this.spyContext(SPY_METHODS)
+            this.hasSpied = true
+        }
+        if (reset) this.calls.length = 0
+    }
+
+    endCapture(onlyDraws = false) {
+        let result = this.calls.slice()
+        this.calls.length = 0
+        if (onlyDraws) result = result.filter(command => command.draw.indexOf('draw') > -1)
+        return result
+    }
+
+    destroy() {
+        this.unspyContext(this.gl)
+        this.gl = null
+    }
+}
+
+function getDetails(gl, methodName, args) {
+    // Get drawcall details
+    let draw = methodName
+
+    if (methodName.toLowerCase().indexOf('draw') > -1) {
+        const mode = DRAW_MODES.filter(mode => args[0] === gl[mode])
+        draw = `${draw}: ${mode}, ${args[1]} indices`
+
+        if (methodName.toLowerCase().indexOf('array') > -1) {
+            const count = args[2]
+            draw = `${draw}, ${count} vertices`
+
+        }
+    }
+
+    // Clear bits details
+    if (methodName === 'clear') {
+        const depth = DEPTH_BUFFER_BIT & args[0]
+        const stencil = STENCIL_BUFFER_BIT & args[0]
+        const color = COLOR_BUFFER_BIT & args[0]
+
+        const bits = ['DEPTH', 'STENCIL', 'COLOR'].filter((value, i) => [depth, stencil, color][i])
+        draw = `${draw}: ${bits.join(', ')}`
+    }
+
+    // Get program name
+    let programName
+    if (NO_PROGRAM.indexOf(methodName) < 0) {
+        const program = gl.getParameter(CURRENT_PROGRAM)
+        if (program) {
+            const shaders = gl.getAttachedShaders(program)
+            const source = gl.getShaderSource(shaders[0])
+            const name = readNameFromShaderSource(source)
+            programName = name || 'Unnamed shader'
+        }
+    }
+    
+    return programName ? { draw, program: programName } : { draw }
+}
+
+// Taken from
+// https://github.com/BabylonJS/Spector.js/blob/5f652baa091b96e79f8d7d98bc6cc1d4e404ad00/src/backend/utils/readProgramHelper.ts#L63C5-L101C2
+// could replace with https://github.com/glslify/glsl-shader-name ?
+function readNameFromShaderSource(source) {
+    try {
+        let name = ''
+        let match
+
+        const shaderNameRegex = /#define[\s]+SHADER_NAME[\s]+([\S]+)(\n|$)/gi
+        match = shaderNameRegex.exec(source)
+        if (match !== null) {
+            if (match.index === shaderNameRegex.lastIndex) {
+                shaderNameRegex.lastIndex++
+            }
+            name = match[1]
+        }
+
+        if (name === '') {
+            // #define SHADER_NAME_B64 44K344Kn44O844OA44O8
+            // #define SHADER_NAME_B64 8J+YjvCfmIE=
+            const shaderName64Regex = /#define[\s]+SHADER_NAME_B64[\s]+([\S]+)(\n|$)/gi
+            match = shaderName64Regex.exec(source)
+            if (match !== null) {
+                if (match.index === shaderName64Regex.lastIndex) {
+                    shaderName64Regex.lastIndex++
+                }
+
+                name = match[1]
+            }
+
+            if (name) {
+                name = decodeURIComponent(atob(name))
+            }
+        }
+
+        return name
+    }
+    catch (e) {
+        return null
+    }
+}
